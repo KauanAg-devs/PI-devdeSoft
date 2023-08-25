39878d853eee68e1e23f39fd5165e7a3b77b17ba
kauanAg-devs <kauan.aguiar@gsuite.iff.edu.br>
2023-08-25 14:21:39 -0300

    commit

--- backend/routes/api.routes.ts
+++ backend/routes/api.routes.ts
@@ -2,8 +2,9 @@ import express from 'express'
 import { Crud } from '../controllers/routers.helpers'
 const Router = express.Router()
  const apiCrud = new Crud()
-Router.get('/api', (req,res)=>{
- apiCrud.post({name: req.query.name as string, password: req.query.password as string})
+Router.get('/api', async (req,res)=>{
+ const user = await apiCrud.post({name: req.query.name as string, password: req.query.password as string})
+ res.status(200).json({message: user})
 })
   
 Router.post('/api', (req,res)=>{
