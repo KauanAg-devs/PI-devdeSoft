299835987ab0db38e32ef23da5887e2057ab32a7
kauanAg-devs <kauan.aguiar@gsuite.iff.edu.br>
2023-08-31 11:53:40 -0300

    GRG

--- backend/index.ts
+++ backend/index.ts
@@ -5,7 +5,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
 const app = express()
-const PORT = process.env.PORT ?? 3000
+const PORT = 3000 ?? process.env.PORT
 app.use(cors())
 app.use(express.json())
  
